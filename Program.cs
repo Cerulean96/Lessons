30c36ce68c38ed8f0c00f02da9a96d9ec67799ac
Cerulean96 <104350347+Cerulean96@users.noreply.github.com>
2022-05-12 11:06:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[3, 5]
+            {
+                {2, 56,17,45,67},
+                {43,56,926,768,2},
+                {61,176, 945,430,44},
+            };
+            
+            foreach (var item in myArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
